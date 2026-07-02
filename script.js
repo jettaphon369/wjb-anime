a70ea737448a0c2657bf70d6b5d33b31a35d7238
jettaphon369 <jettaphon369@gmail.com>
2026-07-02 14:20:55 +0700

    Refactor script.js to handle anime episode selection

--- script.js
+++ script.js
@@ -1,11 +1,55 @@
-const list = document.getElementById("list");
+const params = new URLSearchParams(window.location.search);
+const animeId = params.get("anime") || "one-piece";
 
-data.forEach(a=>{
- const div = document.createElement("div");
- div.className="card";
- div.innerHTML = `
-  <h3>${a.title}</h3>
-  <a href="anime.html?id=${a.id}">ดู</a>
- `;
- list.appendChild(div);
+let currentEP = 1;
+let currentServer = "youtube";
+
+const anime = animeData[animeId];
+
+document.getElementById("title").innerText = anime.title;
+
+// ---------- CREATE EP BUTTONS ----------
+const epList = document.getElementById("ep-list");
+
+Object.keys(anime.eps).forEach(ep => {
+  const btn = document.createElement("button");
+  btn.innerText = "EP " + ep;
+
+  btn.onclick = () => {
+    currentEP = ep;
+    loadServers();
+    loadVideo();
+  };
+
+  epList.appendChild(btn);
 });
+
+// ---------- LOAD SERVER BUTTONS ----------
+function loadServers() {
+  const serverList = document.getElementById("server-list");
+  serverList.innerHTML = "";
+
+  const servers = anime.eps[currentEP].servers;
+
+  Object.keys(servers).forEach(server => {
+    const btn = document.createElement("button");
+    btn.innerText = server;
+
+    btn.onclick = () => {
+      currentServer = server;
+      loadVideo();
+    };
+
+    serverList.appendChild(btn);
+  });
+}
+
+// ---------- LOAD VIDEO ----------
+function loadVideo() {
+  const url = anime.eps[currentEP].servers[currentServer];
+  document.getElementById("videoPlayer").src = url;
+}
+
+// init
+loadServers();
+loadVideo();
