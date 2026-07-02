718c4345d812ff5d92e482dc0481c575a49164c7
jettaphon369 <jettaphon369@gmail.com>
2026-07-02 14:03:33 +0700

    Update data structure for anime titles and episodes

--- data.js
+++ data.js
@@ -1,4 +1,25 @@
 const data = [
- {id:"1", title:"Solo Leveling", eps:[1,2,3]},
- {id:"2", title:"Attack on Titan", eps:[1,2]}
-];
+  {
+    id:"1",
+    title:"One Piece",
+    episodes:[
+      "https://www.youtube.com/embed/dQw4w9WgXcQ",
+      "https://www.youtube.com/embed/dQw4w9WgXcQ",
+      "https://www.youtube.com/embed/dQw4w9WgXcQ"
+    ]
+  },
+  {
+    id:"2",
+    title:"Solo Leveling",
+    episodes:[
+      "https://www.youtube.com/embed/dQw4w9WgXcQ"
+    ]
+  },
+  {
+    id:"3",
+    title:"Attack on Titan",
+    episodes:[
+      "https://www.youtube.com/embed/dQw4w9WgXcQ"
+    ]
+  }
+]
